013362ff24a9ac8ee87dc56c38d4cc4cfdb29266
Naruaki Etomi <naruaki@Mac-Pro.local>
2019-02-01 00:50:20 +0900

    足し算と引き算を追加

--- 9cc.c
+++ 9cc.c
@@ -7,14 +7,35 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  char *p = argv[1];
+
   printf(".global main\n");
   printf(".ent main\n");
   printf("$main..ng:\n");
   printf("main:\n");
+  printf("  lda $1, %ld($31)\n", strtol(p, &p, 10));
 
-  printf("  lda $0, %d($31)\n", atoi(argv[1]));
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("  lda $2, %ld($31)\n", strtol(p, &p, 10));
+      printf("  addl $2,$1,$1\n");
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("  lda $3, %ld($31)\n", strtol(p, &p, 10));
+      printf("  subl $1,$3,$1\n");
+      continue;
+    }
+
+    fprintf(stderr, "Unexpected value '%c'\n", *p);
+    return 1;
+  }
+
+  printf("  mov $1,$0\n");
   printf("  ret\n");
   printf("  .end main\n");
   return 0;
 }
-
